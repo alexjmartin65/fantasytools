666110268992835dbc833f0b9bf6759a2c7f19a3
Alex Martin <alexander.martin45@gmail.com>
2022-08-19 21:12:11 -0500

    pre-react

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ $(document).ready(function(){
     $(".export").click(exportMain);
     $(".hidden").click(toggleHidden);
     $(".hidden-tiers").click(toggleTiers);
+    $(".translate").click(exportUnderdog);
+
+
 });
 
 function toggleContainer() {
@@ -47,7 +50,11 @@ function notMineClicked() {
 
     var playerName = tr.find(".playerName").text().trim();
     strikeTieredPlayer(playerName);
-    tr.slideUp();
+    // tr.slideUp();
+    if (isDefault)
+        tr.addClass("hide-me");
+    else
+        tr.addClass("show-me");
 }
 
 function mineClicked() {
@@ -68,7 +75,10 @@ function mineClicked() {
 
     var playerName = tr.find(".playerName").text().trim();
     strikeTieredPlayer(playerName);
-    tr.slideUp();
+    if (isDefault)
+        tr.addClass("hide-me");
+    else
+        tr.addClass("show-me");
 }
 
 function strikeTieredPlayer(playerName){
@@ -127,15 +137,12 @@ function parseMainCsvData(csvData) {
             mine: false,
             notMine: false,
             neither: true,
-            name: line[0].replace(/"/g, ''),
-            etrRank: line[1],
-            team: line[2],
-            position: line[3] ? line[3].replace(/"/g, '') : '',
-            etrPositionRank: line[4],
-            adp: line[5],
-            adpPositionRank: line[6],
-            adpDiff: line[7],
-            notes: line[8],
+            name: line[3].replace(/"/g, ''),
+            etrRank: line[0],
+            recent: line[1],
+            etrPositionRank: line[2] ? line[2].replace(/"/g, '') : '',
+            position: line[2] ? line[2].replace(/"/g, '')[0] + line[2].replace(/"/g, '')[1] : '',
+            Notes: line[4],
             draftedAt: ""
         });
     }
@@ -248,14 +255,14 @@ function parseTierCsvData(csvData) {
         var line = newLinebrk[i].split(",");
         parsedData.push(line);
 
-        var wrName = line[1].replace(/"/g, '');
-        var wrRank = getRankForPosition(wrName);
-        var rbName = line[2].replace(/"/g, '');;
-        var rbRank = getRankForPosition(rbName);
-        var teName = line[3].replace(/"/g, '');;
-        var teRank = getRankForPosition(teName);
-        var qbName = line[4].replace(/"/g, '');;
-        var qbRank = getRankForPosition(qbName);
+        var wrName = line[6] ? line[6].replace(/"/g, '') : '';
+        var wrRank = line[8] ? line[8].replace(/"/g, '') : '';
+        var rbName = line[3] ? line[3].replace(/"/g, '') : '';
+        var rbRank = line[5] ? line[5].replace(/"/g, '') : '';
+        var teName = line[9] ? line[9].replace(/"/g, '') : '';
+        var teRank = line[11] ? line[11].replace(/"/g, '') : '';
+        var qbName = line[0] ? line[0].replace(/"/g, '') : '';
+        var qbRank = line[2] ? line[2].replace(/"/g, '') : '';
 
         objectData.push({
             rank: line[0],
@@ -326,11 +333,123 @@ function exportMain(){
 
 };
 
+var isDefault = true;
 function toggleHidden() {
-    $( ".table tbody .strikeout").toggle();
-    $( ".table tbody tr.mine").toggle();
+    // $( ".table tbody .strikeout").toggle();
+    // $( ".table tbody tr.mine").toggle();
+
+    if (isDefault){
+        $( ".table tbody .hide-me").addClass("show-me");
+        $( ".table tbody .hide-me").removeClass("hide-me");
+        isDefault = false;
+    }
+    else{
+        $( ".table tbody .show-me").addClass("hide-me");
+        $( ".table tbody .show-me").removeClass("show-me");
+        isDefault = true;
+    }
+    
 }
 
 function toggleTiers() {
     $(".hideTiers").toggle();
+}
+
+function exportUnderdog(){
+    exportUnderdogFile();
+}
+
+var etrCsvData;
+var underdogCsvData;
+
+function exportUnderdogFile(){
+    var etrFile = $("#etrCSV");
+    var etrFileValue = etrFile[0].files[0];
+    var etrReader = new FileReader();
+
+    etrReader.addEventListener('load', function (etrEvent) {
+              
+        etrCsvData = etrEvent.target.result; 
+        $("#etrCSV").val("");
+
+        var underdogFile = $("#underogCSV");
+        var underdogFileValue = underdogFile[0].files[0];
+        var underdogReader = new FileReader();
+
+        underdogReader.addEventListener('load', function (underDogEvent) {
+                
+            underdogCsvData = underDogEvent.target.result; 
+            $("#underogCSV").val("");
+
+            combineAndExport();
+
+
+        });
+
+        underdogReader.readAsBinaryString(underdogFileValue);
+
+
+    });
+
+    etrReader.readAsBinaryString(etrFileValue);
+
+
+}
+
+function combineAndExport() {
+
+    var underdogDictionary = {};
+
+    let underdogLines = underdogCsvData.split("\n");
+    for (var index = 1; index < underdogLines.length; index++){
+        var line = underdogLines[index].split(",");
+        if (!line[1]){
+            continue;
+        }
+
+        var name = line[1].replace(/(\r\n|\n|\r)/gm, "").replace("\"", "").replace("\"", "") + line[2].replace(/(\r\n|\n|\r)/gm, "").replace("\"", "").replace("\"", "");
+        underdogDictionary[name] = line[0];
+    }
+
+    console.log(underdogDictionary.length);
+
+    var exportLines = [];
+    var etrLines = etrCsvData.split("\n");
+
+    var headerRow = [];
+    headerRow.push('id');
+    headerRow.push('name');
+    exportLines.push(headerRow);
+    
+    for (var index = 1; index < etrLines.length; index++){
+        var line = etrLines[index].split(",");
+        if (!line[0]){
+            continue;
+        }
+        var name = line[0].replace(" ", "").replace("\"", "").replace("\"", "");
+        var currentRow = [];
+        if (underdogDictionary[name]){
+            currentRow.push(underdogDictionary[name]);
+            currentRow.push(name); 
+            exportLines.push(currentRow);
+            delete underdogDictionary[name];
+        }
+    }
+
+    for (const property in underdogDictionary) {
+        var currentRow = [];
+        currentRow.push(underdogDictionary[property]);
+        currentRow.push(property);
+        exportLines.push(currentRow);
+    }
+
+    let csvContent = "data:text/csv;charset=utf-8," + exportLines.map(e => e.join(",")).join("\n");
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "mainrankinsExported.csv");
+    document.body.appendChild(link); // Required for FF
+    link.click();
+    document.body.removeChild(link);
+
 }
